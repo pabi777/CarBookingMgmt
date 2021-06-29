560d7019073253dbb97c9b7a298a165823aa00db
pabitra <rpabitra7@gmail.com>
2021-06-29 18:42:52 +0530

    naming convention fixed

--- car.py
+++ car.py
@@ -15,14 +15,13 @@ from copy import deepcopy
 pp = pprint.PrettyPrinter(indent=4)
 
 
-class Frontdesk:
+class Garage:
     added_car = False
     car_dict = {}
 
     def formatter(self, cardict):
         if cardict:
             temp = deepcopy(cardict)
-            print(cardict)
             for item in temp:
                 if temp[item]['duration']:
                     temp[item]['duration'] = temp[item]['duration'].strftime(
@@ -49,32 +48,44 @@ class Frontdesk:
 
 class Available_Maker:
 
-    def __init__(self, frontdesk) -> None:
-        self.frontdesk = frontdesk
+    def __init__(self, garage) -> None:
+        self.garage = garage
 
     def checker(self):
-        if self.frontdesk.added_car:
-            for item in self.frontdesk.car_dict:
-                if self.frontdesk.car_dict[item]['duration']:
-                    if self.frontdesk.car_dict[item]['duration'] < datetime.now():
-                        self.frontdesk.car_dict[item]['status'] = 'AVAILABLE'
-                        self.frontdesk.car_dict[item]['duration'] = ''
+        if self.garage.added_car:
+            for item in self.garage.car_dict:
+                if self.garage.car_dict[item]['duration']:
+                    if self.garage.car_dict[item]['duration'] < datetime.now():
+                        self.garage.car_dict[item]['status'] = 'AVAILABLE'
+                        self.garage.car_dict[item]['duration'] = ''
 
 
-class Customer(Available_Maker):
-
-    def __init__(self, frontdesk) -> None:
-        self.frontdesk = frontdesk
+class Customer():
+    def __init__(self) -> None:
         self.username = str(input('Enter name: '))
         self.phone = str(input('Phone: '))
-        if not self.username or (not len(self.phone)==10):
+        if not self.username or (not len(self.phone) == 10):
             print("invalid details...please provide valid username and phone number. Length of the phonenumber must be 10")
-            self.__init__(frontdesk)
-        super().__init__(frontdesk)
+            self.__init__()
+
+
+class Booking(Available_Maker):
+    def __init__(self, IAM, garage) -> None:
+        self.IAM = IAM
+        self.garage = garage
+        super().__init__(garage)
+
+    def is_customer(func):
+        def inner(self, **kwargs):
+            if isinstance(self.IAM, Customer):
+                func(self, **kwargs)
+            else:
+                print('Unauthorized Access')
+        return inner
 
     def is_any_car_available(func):
         def inner(self, **kwargs):
-            if self.frontdesk.car_dict:
+            if self.garage.car_dict:
                 # if isinstance(self.IAM, Customer):
                 func(self, **kwargs)
             else:
@@ -82,29 +93,31 @@ class Customer(Available_Maker):
                 return('No cars available in stock right now!!')
         return inner
 
+    @is_customer
     @is_any_car_available
     def show_car(self):
-        if self.frontdesk.added_car:
+        if self.garage.added_car:
             self.checker()
             cars = [
-                self.frontdesk.car_dict[x] for x in self.frontdesk.car_dict if self.frontdesk.car_dict[x]['status'] == f'AVAILABLE']
+                self.garage.car_dict[x] for x in self.garage.car_dict if self.garage.car_dict[x]['status'] == f'AVAILABLE']
             car_count = len(cars)
-            pp.pprint(self.frontdesk.list_formatter(cars))
+            pp.pprint(self.garage.list_formatter(cars))
             print(f'\n{car_count} Car available \n')
 
+    @is_customer
     @is_any_car_available
     def request_car(self):
         is_available = True
-        for i, car in enumerate(self.frontdesk.car_dict):
-            if self.frontdesk.car_dict[car]['status'] == 'AVAILABLE' or self.frontdesk.car_dict[car]['duration'] < datetime.now():
+        for i, car in enumerate(self.garage.car_dict):
+            if self.garage.car_dict[car]['status'] == 'AVAILABLE' or self.garage.car_dict[car]['duration'] < datetime.now():
                 self.show_car()
                 ch = str(
                     input('Do you want to book car? y/n: ')).lower()
                 if ch == 'n':
                     is_available = True
                     break
-                self.frontdesk.car_dict[car]['status'] = f'BOOKED to {self.username}'
-                self.frontdesk.car_dict[car]['duration'] = datetime.now(
+                self.garage.car_dict[car]['status'] = f'BOOKED to {self.IAM.username}'
+                self.garage.car_dict[car]['duration'] = datetime.now(
                 )+timedelta(hours=int(os.getenv('time_duration', 2)))
             else:
                 is_available = False
@@ -112,25 +125,25 @@ class Customer(Available_Maker):
         if not is_available:
             print('Sorry! no car can be booked at this time..check again later')
 
+    @is_customer
     def booked_cars(self):
         self.checker()
-        carlist = [self.frontdesk.car_dict[x]
-                   for x in self.frontdesk.car_dict if self.frontdesk.car_dict[x]['status'] == f'BOOKED to {self.username}']
-        print(self.frontdesk.list_formatter(carlist))
+        carlist = [self.garage.car_dict[x]
+                   for x in self.garage.car_dict if self.garage.car_dict[x]['status'] == f'BOOKED to {self.IAM.username}']
+        print(self.garage.list_formatter(carlist))
+
 
 # Note: Assuming passkey is shared only among managers
-
-
 class Manager():
     username, passkey = None, str(os.getenv('secret', 'secret'))
 
 
 class Admin(Available_Maker):
 
-    def __init__(self, IAM, frontdesk):
+    def __init__(self, IAM, garage):
         self.IAM = IAM
-        self.frontdesk = frontdesk
-        super().__init__(frontdesk)
+        self.garage = garage
+        super().__init__(garage)
 
     def is_manager(func):
         def inner(self, **kwargs):
@@ -147,7 +160,7 @@ class Admin(Available_Maker):
         start = i
         while key != 'q':
             # Automatically generating registrarion number
-            self.frontdesk.car_dict.update(
+            self.garage.car_dict.update(
                 {
                     f'WB {i}': {
                         'regnumber': f'WB {i}',
@@ -160,7 +173,7 @@ class Admin(Available_Maker):
 
             print(f'CAR {(i-start)+1} added')
             i += 1
-            self.frontdesk.added_car = True
+            self.garage.added_car = True
             key = str(
                 input('Press q to stop adding or press Enter to add More car\n')).lower()
 
@@ -170,10 +183,10 @@ class Admin(Available_Maker):
         try:
             reg = str(
                 input('Enter Car registrarion number to update status:')).upper()
-            if reg not in self.frontdesk.car_dict:
+            if reg not in self.garage.car_dict:
                 print('Try again..')
             else:
-                self.frontdesk.car_dict[reg]['status'] = str(
+                self.garage.car_dict[reg]['status'] = str(
                     input("Enter new status: Booked/Available: ")).upper()
         except:
             print('Try again.....')
@@ -183,11 +196,11 @@ class Admin(Available_Maker):
         self.view_car()
         try:
             reg = str(input('Enter Car registrarion number to remove: ')).upper()
-            if reg not in self.frontdesk.car_dict:
+            if reg not in self.garage.car_dict:
                 print('Try again..')
             else:
-                print("Removed car details: ", self.frontdesk.single_formatter(
-                    self.frontdesk.car_dict.pop(reg)))
+                print("Removed car details: ", self.garage.single_formatter(
+                    self.garage.car_dict.pop(reg)))
         except:
             print('Try again....')
 
@@ -197,19 +210,19 @@ class Admin(Available_Maker):
         if has_reg_no:
             try:
                 reg_id = str(input('Enter reg number:')).upper()
-                pp.pprint(self.frontdesk.single_formatter(
-                    self.frontdesk.car_dict[reg_id]))
+                pp.pprint(self.garage.single_formatter(
+                    self.garage.car_dict[reg_id]))
             except KeyError:
                 print('No car found corrosponding resgistration number\n')
             except:
                 # raise
                 print("Try again\n")
         else:
-            pp.pprint(self.frontdesk.formatter(self.frontdesk.car_dict))
+            pp.pprint(self.garage.formatter(self.garage.car_dict))
 
 
 if __name__ == "__main__":
-    frontdesk = Frontdesk()
+    garage = Garage()
     while True:
         try:
             choice = int(input("1.Manager login 2.User Login 3.Exit\n"))
@@ -231,7 +244,7 @@ if __name__ == "__main__":
                 print("\nWrong password, Try again.......\n")
             if loggedin:
                 key = None
-                admin = Admin(manager, frontdesk)
+                admin = Admin(manager, garage)
                 while key != 6:
                     try:
                         key = int(input(
@@ -250,7 +263,8 @@ if __name__ == "__main__":
                     elif key == 5:
                         admin.view_car(has_reg_no=True)
         elif choice == 2:
-            customer = Customer(frontdesk)
+            customer = Customer()
+            booking = Booking(customer, garage)
             while True:
                 try:
                     cust_choice = int(
@@ -260,15 +274,15 @@ if __name__ == "__main__":
 
                 msg = ''
                 if cust_choice == 1:
-                    msg = customer.show_car()
+                    msg = booking.show_car()
 
                 elif cust_choice == 2:
                     if not msg == 'No cars available in stock right now!!':
-                        customer.request_car()
+                        booking.request_car()
                     else:
                         print(msg)
                 elif cust_choice == 3:
-                    customer.booked_cars()
+                    booking.booked_cars()
                 elif cust_choice == 4:
                     break
 
