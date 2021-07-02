ac69ebb64bac01f28dd854172ab61f62a78ce831
Pabitra Roy <45242805+pabi777@users.noreply.github.com>
2021-07-02 15:17:17 +0530

    Delete s.py

--- s.py
+++
@@ -1,8 +0,0 @@
-['Lamborghini Diablo',
- 'Ford Raptor',
- 'Ferrari Testarossa',
- 'Porsche 911 Carrera',
- 'Jensen Interceptor',
- 'Lamborghini Huracán',
- 'Ferrari 812 Superfast',
- 'Jeep Gladiator']
